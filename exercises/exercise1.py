f91e8e71ece82ee6830b2da9894bb7094c5583ba
Weronika Stasiak <82800@student.ue.poznan.pl>
2024-03-13 10:58:12 +0100

    dodanie trójkąta

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -3,3 +3,10 @@ bok_a = 4
 pole_kw = bok_a * bok_a
 
 print("Pole kwadratu wynosi: " + str(pole_kw))
+
+bok_b = 5
+bok_h = 4
+
+pole_tr = (bok_b * bok_h)/2
+
+print("Pole trójkąta: " + str(pole_tr))
